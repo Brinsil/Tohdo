2434702f375121fd1d30704347f0dc7c84c9d442
Brinsil Elias <brinsilelias01@gmail.com>
2022-10-21 19:12:26 +0530

    Create tohdo model file a add Tohdo class

---
+++ dev_utils/tohdo_model.py
@@ -0,0 +1,33 @@
+# Creating class for the model
+
+from datetime import datetime
+
+class Tohdo:
+    def __init__(self, id, task, section, date_completed = None, status = 'Todo') -> None:
+        self.id = id
+        self.task = task
+        self.section = section
+        self.date_added = datetime.now().isoformat()
+        self.date_completed = date_completed
+        self.status = status
+
+    def get_id():
+        return self.id
+
+    def get_section():
+        return self.section
+
+    def get_date_added():
+        return self.date_added
+
+    def get_date_completed():
+        return self.date_completed
+
+    def get_status():
+        return self.status
+
+    def __repr__(self) -> str:
+        return f'{self.id}, {self.task}, {self.section}, {self.date_added}, {self.date_completed}, {self.status}'
+
+
+
